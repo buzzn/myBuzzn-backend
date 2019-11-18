d735c69c99c9cc79654db80eea9ca4f79dba373e
Clemens Manert <Clemens@Manert.de>
2019-11-18 15:02:36 +0100

    Error handler for unkown Resources.

---
+++ util/error.py
@@ -0,0 +1,11 @@
+import json
+
+
+class Error:
+    """Represents an error which occurred in the application."""
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def to_json(self):
+        return json.dumps(self.__dict__)
