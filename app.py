3a9c3f75610040da5d57085b846a1002e97289d2
Clemens Manert <Clemens@Manert.de>
2019-11-21 19:01:08 +0100

    Add database access to app.

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from os import environ
 
 from setup_app import setup_app
 
+
 class RunConfig():
     """Graps app parameters from the environment."""
     SECRET_KEY = environ.get('BUZZN_SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = environ.get('BUZZN_SQLALCHEMY_DATABASE_URI')
+
 
 app = setup_app(RunConfig())
 
