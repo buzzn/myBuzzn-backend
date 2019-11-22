cfe4b5c582baef602665fd20050f8c54738de80a
Clemens Manert <Clemens@Manert.de>
2019-11-22 14:34:41 +0100

    Adds 404 error handler to app

--- app.py
+++ app.py
@@ -2,12 +2,20 @@ from os import environ
 
 from flask import Flask
 
+from util.error import Error
+
 app = Flask(__name__)
 
 # Config
 app.config.update({
     'SECRET_KEY': environ.get('BUZZN_SECRET_KEY')
 })
+
+# Routes
+@app.errorhandler(404)
+def not_found(error):
+    return (Error('Unknown path', 'This ressource is not available. '+error).to_json(),
+            status.HTTP_404_NOT_FOUND)
 
 if __name__ == "__main__":
     app.run()
