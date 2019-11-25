249404426aa11f69c9a7255e274819d473a41941
Clemens Manert <Clemens@Manert.de>
2019-11-25 15:32:21 +0100

    Share config between unittests and use the same setup as a running flask instance

--- app.py
+++ app.py
@@ -2,15 +2,11 @@ from os import environ
 
 from setup_app import setup_app
 
-from util.error import Error
-
-
 class RunConfig():
     """Graps app parameters from the environment."""
     SECRET_KEY = environ.get('BUZZN_SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = environ.get('BUZZN_SQLALCHEMY_DATABASE_URI')
-
 
 app = setup_app(RunConfig())
 
@@ -19,7 +15,6 @@ app = setup_app(RunConfig())
 def not_found(error):
     return (Error('Unknown path', 'This ressource is not available. '+error).to_json(),
             status.HTTP_404_NOT_FOUND)
-
 
 if __name__ == "__main__":
     app.run()
