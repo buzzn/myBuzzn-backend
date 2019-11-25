9dff36c691af7c96577267b34652e83efd3e9e61
Clemens Manert <Clemens@Manert.de>
2019-11-25 15:32:21 +0100

    Share config between unittests and use the same setup as a running flask instance

--- apitests/error_handler_test.py
+++ apitests/error_handler_test.py
@@ -1,18 +1,15 @@
-from flask import Flask
-from flask_testing import TestCase
 from flask_api import status
 
+from apitests.buzzn_test_case import BuzznTestCase
+from util.error import UNKNOWN_RESSOURCE
 
-class ErrorHandlerTest(TestCase):
+
+class ErrorHandlerTest(BuzznTestCase):
     """Checks whether an unknown route results in an HTTP 404 not found."""
 
-    def create_app(self):
-        app = Flask(__name__)
-        app.config['TESTING'] = True
-        app.config['LIVESERVER_PORT'] = 0
-
-        return app
-
     def test_unknown_route(self):
-        response = self.client.get("/non_existing_route/")
+        response = self.client.get('/bam')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.json['name'], UNKNOWN_RESSOURCE.name)
+        self.assertEqual(response.json['description'],
+                         UNKNOWN_RESSOURCE.description)
--- util/error.py
+++ util/error.py
@@ -1,6 +1,5 @@
 import json
 
-
 class Error:
     """Represents an error which occurred in the application."""
     def __init__(self, name, description):
@@ -9,3 +8,5 @@ class Error:
 
     def to_json(self):
         return json.dumps(self.__dict__)
+
+UNKNOWN_RESSOURCE = Error('Unknown ressource', 'This ressource is not available.')
