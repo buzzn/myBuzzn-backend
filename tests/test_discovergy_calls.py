5e5715e907acdf0e8d035024bed2d35892e98628
Judith Greif <obermaedchen@gmail.com>
2019-11-28 17:28:30 +0100

    Unit tests for IndividualConsumptionHistory

--- tests/test_discovergy_calls.py
+++ tests/test_discovergy_calls.py
@@ -1,12 +1,14 @@
 from unittest import mock
+import discovergy
 from tests.buzzn_test_case import BuzznTestCase
 
 
 class IndividualConsumptionHistoryTestCase(BuzznTestCase):
     """ Unit tests for route IndividualConsumptionHistory. """
 
-    def test_route_exists(self):
+    @mock.patch('discovergy.discovergy.Discovergy.login', return_value=True)
+    def test_route_exists(self, login):
         """ Check whether route '/individual-consumption-history' exists. """
 
         response = self.client.get('/individual-consumption-history')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 206)
