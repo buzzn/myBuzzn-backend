da4d987ef86833553c998df4d6706271c4d9b631
Judith Greif <obermaedchen@gmail.com>
2019-11-29 12:05:30 +0100

    Unit tests for IndividualConsumptionHistory

--- tests/test_discovergy_calls.py
+++ tests/test_discovergy_calls.py
@@ -1,5 +1,16 @@
 from unittest import mock
+from flask_api import status
 from tests.buzzn_test_case import BuzznTestCase
+
+
+READINGS = [{'time': 1574244000000, 'values': {'power': 0, 'power3': -27279,
+                                               'energyOut': 0, 'power1': 0,
+                                               'energy': 2180256872214000,
+                                               'power2': -2437}},
+            {'time': 1574247600000, 'values': {'power': 0, 'power3': -25192,
+                                               'energyOut': 0, 'power1': 0,
+                                               'energy': 2180256872214000,
+                                               'power2': -2443}}]
 
 
 class IndividualConsumptionHistoryTestCase(BuzznTestCase):
@@ -8,8 +19,10 @@ class IndividualConsumptionHistoryTestCase(BuzznTestCase):
     # pylint does not get the required argument from the @mock.patch decorator
     # pylint: disable=unused-argument
     @mock.patch('discovergy.discovergy.Discovergy.login', return_value=True)
-    def test_route_exists(self, login):
+    @mock.patch('discovergy.discovergy.Discovergy.get_readings',
+                return_value=READINGS)
+    def test_route_exists(self, login, get_readings):
         """ Check whether route '/individual-consumption-history' exists. """
 
-        # response = self.client.get('/individual-consumption-history')
-        # self.assertEqual(response.status_code, 206)
+        response = self.client.get('/individual-consumption-history')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
