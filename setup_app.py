60594c3ca695307ea9f268a4eeca09de42194814
Clemens Manert <Clemens@Manert.de>
2019-11-29 12:01:14 +0100

    Apply judith's code review.

--- setup_app.py
+++ setup_app.py
@@ -4,18 +4,14 @@ from routes.consumption_history import GroupConsumptionHistory
 from routes.disaggregation import IndividualDisaggregation
 from routes.disaggregation import GroupDisaggregation
 
-from util.error import UNKNOWN_RESSOURCE
+from util.error import UNKNOWN_RESOURCE
 
 
 def setup_app(app_config):
     """Create an app using the given config.
-
-    Arguments:
-        app_config {object} -- An object containing all the environmental
-        parameters.
-
-    Returns:
-        flask app -- The created app, ready to run.
+    :param object app_config: An object containing all the environmental
+    parameters.
+    :return: The created app, ready to run.
     """
     app = Flask(__name__)
     app.config.from_object(app_config)
@@ -36,11 +32,11 @@ def setup_app(app_config):
     app.register_blueprint(IndividualDisaggregation)
     app.register_blueprint(GroupDisaggregation)
 
-    # Routes are called by the user, they are actually used.
-    # pylint: disable=unused-variable
+    # Routes are called by the user, so they are actually used.
+    #pylint: disable=unused-variable
     @app.errorhandler(404)
     def not_found(error):
-        return (UNKNOWN_RESSOURCE.to_json(),
+        return (UNKNOWN_RESOURCE.to_json(),
                 error.code)
 
     return app
