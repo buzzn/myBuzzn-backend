057ee0b788d49d290e77c98e1ea2a70f4efc0037
Judith Greif <obermaedchen@gmail.com>
2019-12-05 12:04:43 +0100

    Refine self-sufficiency calculation and data package creation

--- websocket.py
+++ websocket.py
@@ -1,12 +1,19 @@
 import logging
-import os
 from threading import Lock
-from datetime import datetime, date
+from datetime import datetime, timedelta
 from flask import jsonify
 from flask_socketio import SocketIO
 
 
 logger = logging.getLogger(__name__)
+
+# pylint: disable=fixme
+# TODO - Set/get meter ID in/from database (user meter)
+# TODO - Set/get group meter ID in/from database (common meter)
+# TODO - Set/get user ID in/from database (user)
+# TODO - Set/get group meter IDs in/from database (other users and their meter
+# IDs from group)
+# TODO - discovergy login
 
 
 class Websocket:
@@ -20,43 +27,41 @@ class Websocket:
         self.socketio = SocketIO(app, async_mode=self._async_mode)
         self.d = d
 
-    def create_data(self):
-        """ Creates a data package with the latest discovergy readings. """
-
-        # pylint: disable=fixme
-        # TODO - Set/get meter ID in/from database (user meter)
-        # TODO - Set/get group meter ID in/from database (common meter)
-        # TODO - Set/get user ID in/from database (user)
-        # TODO - Set/get group IDs in/from database (other users from group)
-        # TODO - Set/get group meter IDs in/from database (other users' meters)
-        group_ids = {{"id": 1, "meter_id": '52d7c87f8c26433dbd095048ad30c8cf'},
-                     {"id": 2, "meter_id": '117154df05874f41bfdaebcae6abfe98'}}
+    def self_sufficiency(self, user_id, meter_id):
+        """ Calculate the self-suffiency value for the given user. """
 
         now = round(datetime.now().timestamp() * 1e3)
-        year = datetime.today().year
-        begin = round(datetime(year, 1, 1).timestamp() * 1e3)
+        begin = round((datetime.now() - timedelta(days=365)).timestamp() * 1e3)
+        individual_first_reading = self.d.get_readings(
+            meter_id, begin, now, 'one_month')
+
+        return 0.5
+
+    def create_data(self, meter_id, group_meter_id, user_id, group_meter_ids):
+        """ Creates a data package with the latest discovergy readings. """
+
+        now = round(datetime.now().timestamp() * 1e3)
         try:
-            group_last_reading = self.d.get_last_reading(
-                os.environ['GROUP_METER_ID'])
+            group_last_reading = self.d.get_last_reading(group_meter_id)
             groupConsumption = group_last_reading.get('values').get('energy')
             groupProduction = group_last_reading.get('values').get('energyOut')
-            individual_first_reading = self.d.get_readings(
-                os.environ['METER_ID'], begin, now, 'one_year')
-            individual_last_reading = self.d.get_last_reading(
-                os.environ['METER_ID'])
-            selfSufficiency = individual_last_reading.get(
-                'energy') - individual_first_reading.get('values').get('energy')
+            individual_last_reading = self.d.get_last_reading(meter_id)
             usersConsumption = []
-            usersConsumption.append(dict(
-                id=os.environ['USER_ID'], consumption=individual_last_reading.get('values').get('energy')))
-            for user in group_ids:
-                reading = self.d.get_last_reading(
-                    user.get('meter_id'))
+            usersConsumption.append(dict(id=user_id,
+                                         consumption=individual_last_reading.
+                                         get('values').get('energy')))
+            for user in group_meter_ids:
+                reading = self.d.get_last_reading(user.get('meter_id'))
                 usersConsumption.append(dict(id=user.get('id'),
                                              consumption=reading.get('values').get('energy')))
-            data = dict(date=date, groupConsumption=groupConsumption,
+            data = dict(date=now,
+                        groupConsumption=groupConsumption,
                         groupProduction=groupProduction,
-                        selfSufficiency=selfSufficiency, usersConsumption=usersConsumption)
+                        selfSufficiency=self.self_sufficiency(
+                            user_id, meter_id),
+                        usersConsumption=usersConsumption)
+            print(data)
+            print(jsonify(data))
             return jsonify(data)
 
         except Exception as e:
