4effdd9c1921190ca277f378d3bf4d1975453fa0
Judith Greif <obermaedchen@gmail.com>
2019-12-05 15:56:26 +0100

    Sufficiency calculation

--- websocket.py
+++ websocket.py
@@ -1,18 +1,17 @@
 import logging
 from threading import Lock
 from datetime import datetime, timedelta
-from flask import jsonify
 from flask_socketio import SocketIO
 
 
 logger = logging.getLogger(__name__)
 
 # pylint: disable=fixme
-# TODO - Set/get meter ID in/from database (user meter)
-# TODO - Set/get group meter ID in/from database (common meter)
 # TODO - Set/get user ID in/from database (user)
 # TODO - Set/get group meter IDs in/from database (other users and their meter
 # IDs from group)
+# TODO - Set/get people in flat in/from database
+# TODO - Set/get flat size in/from database
 # TODO - discovergy login
 
 
@@ -27,15 +26,41 @@ class Websocket:
         self.socketio = SocketIO(app, async_mode=self._async_mode)
         self.d = d
 
-    def self_sufficiency(self, user_id, meter_id):
-        """ Calculate the self-suffiency value for the given user. """
+    def self_sufficiency(self, meter_id, inhabitants, flat_size):
+        """ Calculate a user's self-suffiency value the past year as a value
+        between 0 and 1 where 1 is optimal and 0 is worst. Self-sufficiency is
+        defined as (inhabitants * flat size)/(energy value today - energy
+        value one year ago)
+        :param str meter_id: the user's meter id
+        :param int inhabitants: number of people in the user's flat
+        :param float flat_size: the user's flat size
+        :return: the user's self-sufficiency value, 0.0 if there is no history
+        :rtype: float
+        """
 
         now = round(datetime.now().timestamp() * 1e3)
         begin = round((datetime.now() - timedelta(days=365)).timestamp() * 1e3)
-        individual_first_reading = self.d.get_readings(
-            meter_id, begin, now, 'one_month')
 
-        return 0.5
+        try:
+            # Get energy value today
+            individual_last_reading = self.d.get_last_reading(meter_id)
+            energy_today = individual_last_reading.get('values').get('energy')
+
+            # Get energy value one year ago
+            individual_first_reading = self.d.get_readings(meter_id, begin, now,
+                                                           'one_year')
+            energy_one_year_ago = individual_first_reading[0].get(
+                'values').get('energy')
+
+            # Return result
+            return (float(inhabitants) * flat_size)/(float(energy_today)
+                                                     - float(energy_one_year_ago))
+
+        except Exception as e:
+            logger.error("Exception: %s", e)
+
+            # Return result
+            return 0.0
 
     def create_data(self, meter_id, group_meter_id, user_id, group_meter_ids):
         """ Creates a data package with the latest discovergy readings. """
@@ -54,16 +79,15 @@ class Websocket:
                 reading = self.d.get_last_reading(user.get('meter_id'))
                 usersConsumption.append(dict(id=user.get('id'),
                                              consumption=reading.get('values').get('energy')))
-            data = dict(date=now,
+
+            self.self_sufficiency(user_id, meter_id)
+            return dict(date=now,
                         groupConsumption=groupConsumption,
                         groupProduction=groupProduction,
                         selfSufficiency=self.self_sufficiency(
                             user_id, meter_id),
                         usersConsumption=usersConsumption)
-            print(data)
-            print(jsonify(data))
-            return jsonify(data)
 
         except Exception as e:
             logger.error("Exception: %s", e)
-            return jsonify({})
+            return {}
