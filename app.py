56b945a08f9c2ac72c1cb3212b8b4a70b235e2e3
Judith Greif <obermaedchen@gmail.com>
2019-12-12 11:02:31 +0100

    Run app in debug mode

--- app.py
+++ app.py
@@ -20,9 +20,10 @@ class RunConfig():
     BUZZN_BASE_URL = environ.get('BUZZN_BASE_URL')
     BUZZN_MAILER = environ.get('BUZZN_MAILER')
 
+
 app = setup_app(RunConfig())
 d = Discovergy(app.config['CLIENT_NAME'])
 
 if __name__ == "__main__":
     websocket = Websocket(app, "eventlet", d)
-    websocket.socketio.run(app)
+    websocket.socketio.run(app, debug=True)
