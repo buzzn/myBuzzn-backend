387b875ccb367c4f9ec5a02d5d7427d4158e8b77
Judith Greif <obermaedchen@gmail.com>
2019-12-19 15:10:28 +0100

    Fix linter errors

--- util/task.py
+++ util/task.py
@@ -1,6 +1,6 @@
 import os
 import time
-from datetime import datetime, timedelta
+# from datetime import datetime, timedelta
 import logging
 from discovergy.discovergy import Discovergy
 import eventlet
