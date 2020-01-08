81256b87bd242a47abc24946a4b27f6da27beed1
Judith Greif <obermaedchen@gmail.com>
2020-01-08 13:12:04 +0100

    Refactorings, one-time and periodic discovergy data retrieval in task worker

--- util/task.py
+++ util/task.py
@@ -26,6 +26,38 @@ def create_session():
     return session
 
 
+def get_all_meter_ids(session):
+    """ Get all meter ids from sqlite database. """
+
+    return [meter_id[0] for meter_id in session.query(User.meter_id).all()]\
+        + [group_meter_id[0]
+           for group_meter_id in session.query(Group.group_meter_id).all()]
+
+
+def calc_end():
+    """ Calculate timestamp of end of interval. """
+
+    return round(datetime.now().timestamp() * 1e3)
+
+
+def calc_one_year_back():
+    """ Calculate timestamp of one year back in time. """
+
+    return round((datetime.now() - timedelta(days=365)).timestamp() * 1e3)
+
+
+def calc_one_week_back():
+    """ Calculate timestamp of one week back in time. """
+
+    return round((datetime.now() - timedelta(days=7)).timestamp() * 1e3)
+
+
+def calc_two_days_back():
+    """ Calculate timestamp of 24 hours back in time. """
+
+    return round((datetime.now() - timedelta(hours=48)).timestamp() * 1e3)
+
+
 class Task:
     """ Handle discovergy login, data retrieval, populating and updating the
     redis database. """
@@ -41,11 +73,8 @@ class Task:
         self.d.login(email, password)
 
     def populate_redis(self):
-        """ Populate the redis database with all discovergy data from the past.
-        :param redis.Redis redis_db: the app's redis database client
-        :param list all_meter_ids: the meter ids of all users and the group meter
-        ids of all groups from the sqlite database
-        """
+        """ Populate the redis database with all discovergy data from the past. """
+
         all_readings = list()
         all_disaggregations = list()
 
@@ -56,13 +85,10 @@ class Task:
             # Connect to sqlite database
             session = create_session()
 
-            all_meter_ids = [meter_id[0] for meter_id in session.query(User.meter_id).all(
-            )] + [group_meter_id[0] for group_meter_id in session.query(Group.group_meter_id).all()]
-            end = round(datetime.now().timestamp() * 1e3)
-            one_year_back = round(
-                (datetime.now() - timedelta(days=365)).timestamp() * 1e3)
-            one_week_back = round(
-                (datetime.now() - timedelta(days=7)).timestamp() * 1e3)
+            all_meter_ids = get_all_meter_ids(session)
+            end = calc_end()
+            one_year_back = calc_one_year_back()
+            one_week_back = calc_one_week_back()
 
             # Authenticate against the discovergy backend
             self.login()
@@ -95,18 +121,47 @@ class Task:
         except Exception as e:
             logger.error("Exception: %s", e)
 
-    # pylint: disable=no-self-use
     def update_redis(self):
         """ Update the redis database every 60s with the latest discovergy
         data. """
-        while True:
-            print('Updating redis database with the latest discovergy data.')
-            time.sleep(60)
 
-            # pylint: disable=fixme
-            # TODO - Get last reading for all meters
-            # TODO - Get latest disaggregation
-            # TODO - Write all values to redis database
+        while True:
+            time.sleep(60)
+            latest_readings = list()
+            latest_disaggregations = list()
+
+            try:
+                # Connect to sqlite database
+                session = create_session()
+
+                all_meter_ids = get_all_meter_ids(session)
+                end = calc_end()
+                two_days_back = calc_two_days_back()
+                for meter_id in all_meter_ids:
+
+                    # Get last reading for all meters
+                    reading = self.d.get_last_reading(meter_id)
+                    latest_readings.append(dict(meter_id=meter_id,
+                                                time=reading['time'],
+                                                values=reading['values']))
+
+                    # Get latest disaggregation for all meters
+                    disaggregation = self.d.get_disaggregation(
+                        meter_id, two_days_back, end)
+                    timestamps = sorted(disaggregation.keys())
+                    if len(timestamps) > 0:
+                        latest_disaggregations.append(dict(meter_id=meter_id,
+                                                           time=timestamps[-1],
+                                                           values=disaggregation[timestamps[-1]]))
+
+                print(latest_readings)
+                print(latest_disaggregations)
+
+                # pylint: disable=fixme
+                # TODO - Write all values to redis database
+
+            except Exception as e:
+                logger.error("Exception: %s", e)
 
 
 if __name__ == '__main__':
