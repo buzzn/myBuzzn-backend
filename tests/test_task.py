8c4072b6bb76893fc0c1bbc1bf75e94caf385404
Judith Greif <obermaedchen@gmail.com>
2020-01-22 15:45:12 +0100

    Unit tests for function get_all_meter_ids()

--- tests/test_task.py
+++ tests/test_task.py
@@ -1,11 +1,18 @@
+import json
 from unittest import mock
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from models.user import User, GenderType, StateType
 from models.group import Group
 from tests.buzzn_test_case import BuzznTestCase
 from util.database import db
-from util.task import create_session, get_all_meter_ids, calc_end,\
+from util.task import get_all_meter_ids, calc_end,\
     calc_one_year_back, calc_support_year_start, calc_support_week_start,\
     calc_one_week_back, calc_two_days_back, Task
+
+
+ALL_METER_IDS = ['dca0ec32454e4bdd9ed719fbc9fb75d6', '6fdbd41a93d8421cac4ea033203844d1',
+                 'bf60438327b1498c9df4e43fc9327849', '0a0f65e992c042e4b86956f3f080114d']
 
 
 class TaskTestCase(BuzznTestCase):
@@ -17,7 +24,7 @@ class TaskTestCase(BuzznTestCase):
         db.drop_all()
         db.create_all()
         test_user = User(GenderType.MALE, 'Some', 'User', 'test@test.net',
-                         'TestToken', 'dca0ec32454e4bdd9ed719fbc9fb75d6')
+                         'TestToken', 'dca0ec32454e4bdd9ed719fbc9fb75d6', 1)
         test_user.flat_size = 60.0
         test_user.inhabitants = 2
         test_user.set_password('some_password')
@@ -26,10 +33,21 @@ class TaskTestCase(BuzznTestCase):
         db.session.add(User(GenderType.FEMALE, 'judith', 'greif', 'judith@buzzn.net',
                             'TestToken2', '6fdbd41a93d8421cac4ea033203844d1',
                             1))
-        db.session.add(User(GenderType.MALE, 'danny', 'stay', 'danny@buzzn.net',
+        db.session.add(User(GenderType.MALE, 'danny', 'stey', 'danny@buzzn.net',
                             'TestToken3', 'bf60438327b1498c9df4e43fc9327849', 1))
         db.session.add(Group('TestGroup',
                              '0a0f65e992c042e4b86956f3f080114d'))
         db.session.commit()
         self.client.post('/login', data=json.dumps({'user': 'test@test.net',
                                                     'password': 'some_password'}))
+
+    def test_get_all_meter_ids(self):
+        """ Unit tests for function get_all_meter_ids(). """
+
+        result = get_all_meter_ids(db.session)
+
+        # Check return type
+        self.assertTrue(isinstance(result, list))
+
+        # Check return values
+        self.assertEqual(result, ALL_METER_IDS)
