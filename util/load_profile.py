142ab873e9bb7aaf785cf7dcbffe8dbb310c5700
Judith Greif <obermaedchen@gmail.com>
2020-01-24 11:42:46 +0100

    Intermediate state

--- util/load_profile.py
+++ util/load_profile.py
@@ -1,10 +1,23 @@
 import csv
-from util.database import db
+from pathlib import Path
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+# from util.database import db
+
+
+def create_session():
+    parent_dir = Path(__file__).parent.parent.absolute()
+    dbPath = str(parent_dir) + '/mybuzzn.db'
+    engine = create_engine('sqlite:///%s' % dbPath)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
 
 
 def run():
     with open('./load_profiles/standardlastprofil-haushalt-2020.csv') as csvfile:
-        tbl_reader = csv.reader(csvfile, delimiter=',')
+        # tbl_reader = csv.reader(csvfile, delimiter=',')
+        create_session()
         # for row in tbl_reader:
 
 
