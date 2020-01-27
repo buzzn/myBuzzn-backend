a67b52712af2ab5bd1c277e052d6350c41439066
Judith Greif <obermaedchen@gmail.com>
2020-01-27 11:57:33 +0100

    Cleanup code, skip csv header, read all csv values into database

--- util/load_profile.py
+++ util/load_profile.py
@@ -2,8 +2,6 @@ import csv
 from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-# from sqlalchemy.schema import MetaData
-# from util.database import db
 from models.loadprofile import LoadProfile
 
 
@@ -11,7 +9,6 @@ def create_session():
     parent_dir = Path(__file__).parent.parent.absolute()
     dbPath = str(parent_dir) + '/mybuzzn.db'
     engine = create_engine('sqlite:///%s' % dbPath)
-    print(engine.table_names())
     Session = sessionmaker(bind=engine)
     session = Session()
     return session
@@ -20,13 +17,11 @@ def create_session():
 def run():
     with open('./load_profiles/standardlastprofil-haushalt-2020.csv') as csvfile:
         tbl_reader = csv.reader(csvfile, delimiter=',')
+        next(tbl_reader, None)
         session = create_session()
-
-        # TODO
-        session.execute(LoadProfile.insert(), {"date": "1/1/2020", "time":
-                                               "0:15:00", "energy": 27.135})
-
-        # for row in tbl_reader:
+        for row in tbl_reader:
+            session.add(LoadProfile(row[0], row[1], float(row[2])))
+            session.commit()
 
 
 if __name__ == '__main__':
