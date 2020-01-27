61e30b03a42c7ec6c27327dc331293fe9012f622
Judith Greif <obermaedchen@gmail.com>
2020-01-27 13:01:46 +0100

    Error logging

--- util/load_profile.py
+++ util/load_profile.py
@@ -1,8 +1,15 @@
 import csv
+import logging
 from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models.loadprofile import LoadProfile
+
+
+logging.basicConfig()
+logger = logging.getLogger('util/load_profile')
+logging.getLogger().setLevel(logging.INFO)
+exception_template = "An exception of type {0} occurred. Arguments:\n{1!r}"
 
 
 def create_session():
@@ -15,13 +22,19 @@ def create_session():
 
 
 def run():
+
     with open('./load_profiles/standardlastprofil-haushalt-2020.csv') as csvfile:
         tbl_reader = csv.reader(csvfile, delimiter=',')
         next(tbl_reader, None)
         session = create_session()
-        for row in tbl_reader:
-            session.add(LoadProfile(row[0], row[1], float(row[2])))
-            session.commit()
+        try:
+            for row in tbl_reader:
+                session.add(LoadProfile(row[0], row[1], float(row[2])))
+                session.commit()
+
+        except Exception as e:
+            message = exception_template.format(type(e).__name__, e.args)
+            logger.error(message)
 
 
 if __name__ == '__main__':
