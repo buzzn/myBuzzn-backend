1837d5437ff77df10a5f56e5bb470afe2bf00807
Judith Greif <obermaedchen@gmail.com>
2020-01-30 17:19:18 +0100

    Retrieve last reading of a given date from redis database

--- util/energy_saving_calculation.py
+++ util/energy_saving_calculation.py
@@ -1,3 +1,4 @@
+
 # energy_consumption_last_term: The last meter reading minus the first meter
 # reading of the previous term.
 # energy_consumption_ongoing_term: The latest meter reading minus the first meter
@@ -13,9 +14,12 @@
 # The algorithm may run once a day and store the result for each user.
 
 
-from datetime import datetime
+from datetime import datetime, time
+from dateutil import parser
+import json
 import os
 from pathlib import Path
+import pytz
 import logging
 import redis
 from sqlalchemy import create_engine
@@ -28,6 +32,7 @@ logging.getLogger().setLevel(logging.INFO)
 redis_host = os.environ['REDIS_HOST']
 redis_port = os.environ['REDIS_PORT']
 redis_db = os.environ['REDIS_DB']
+redis_client = redis.Redis(host=redis_host, port=redis_port, db=redis_db)
 
 
 def get_engine():
@@ -76,28 +81,65 @@ def calc_ratio_values(start):
     return ratio_values
 
 
-def calc_energy_consumption_last_term(start, meter_id):
+def get_sorted_keys(meter_id):
+    """ Return all keys stored in the redis db for a given meter id.
+    :param str meter_id: the meter id to prefix the scan with
+    """
+
+    return sorted([key.decode('utf-8') for key in redis_client.scan_iter(meter_id + '*')])
+
+
+def get_last_reading_of_date(meter_id, date):
+    """ Return the last reading for the given meter id on the given day which
+    is stored in the redis database. As we were using unix timestamps as basis
+    for our dates all along, there is no need to convert the given,
+    timezone-unaware date to UTC.
+    :param str meter_id: the meter id for which to get the value
+    :param datetime.date date: the date for which to get the value
+    :return: the last reading for the given meter id on the given day
+    :rtype: float
+    """
+
+    readings = []
+    naive_begin = datetime.combine(date, time(0, 0, 0))
+    naive_end = datetime.combine(date, time(23, 59, 59))
+    timezone = pytz.timezone('UTC')
+    begin = (timezone.localize(naive_begin)).timestamp()
+    end = (timezone.localize(naive_end)).timestamp()
+    for key in get_sorted_keys(meter_id):
+        data = json.loads(redis_client.get(key))
+        if data.get('type') == 'reading':
+            reading_date = parser.parse(key[len(meter_id)+1:])
+            reading_timestamp = reading_date.timestamp()
+            if begin <= reading_timestamp <= end:
+                readings.append(data.get('values')['energy'])
+    return readings[-1]
+
+
+def calc_energy_consumption_last_term(meter_id, start):
     """ Calculate the last meter reading minus the first meter reading of the
     previous term for a given meter id.
-    :param datetime.date start: the start value of the ongoing term
     :param str meter_id: the meter id
+    :param datetime.date start: the start value of the ongoing term
     :return: the last meter reading minus the first meter reading of the given
     meter id
     :rtype: float
     """
+
     print(start)
     print(meter_id)
 
 
-def calc_energy_consumption_ongoing_term(start, meter_id):
+def calc_energy_consumption_ongoing_term(meter_id, start):
     """ Calculate the latest meter reading minus the first meter reading of the
     ongoing term for a given meter id.
-    :param datetime.date start: the start value of the ongoing term
     :param datetime meter_id: the meter id
+    :param datetime.date start: the start value of the ongoing term
     :return: the latest meter reading minus the first meter reading of the
     given meter id
     :rtype: float
     """
+
     print(start)
     print(meter_id)
 
@@ -151,30 +193,14 @@ class Task:
         self.redis_client = redis.Redis(host=redis_host, port=redis_port,
                                         db=redis_db)  # connect to server
 
-    def calculate_energy_saving_one_user(self, start, meter_id):
-        """ Calculate the estimated energy saving for one user in a given term
-        using the standard load profile.
-        :param datetime.date start: the start of the term
-        :param str meter_id: the user's meter id
-        :returns: the estimated energy saving for all users
-        :rtype: int
-        """
-
-    def calculate_energy_saving_all_users(self, start):
-        """ Calculate the estimated energy saving for all users in a given term
-        using the standard load profile.
-        :param datetime.date start: the start of the term
-        :returns: the estimated energy saving for all users
-        :rtype: int
-        """
-
 
 def run():
     """ Runs the task which calculates the estimated energy saving for all
     users and writes the results to the redis database. """
 
     # task = Task()
-    calc_ratio_values(datetime(2020, 1, 1).date())
+    date = datetime(2020, 1, 30).date()
+    get_last_reading_of_date('b4234cd4bed143a6b9bd09e347e17d34', date)
 
 
 if __name__ == '__main__':
