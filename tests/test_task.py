560804f234934780f219ccbc38252b40328cd967
Judith Greif <obermaedchen@gmail.com>
2020-02-07 11:07:53 +0100

    Unit tests for new function util.task.calc_timestamps()

--- tests/test_task.py
+++ tests/test_task.py
@@ -6,7 +6,7 @@ from models.user import User, GenderType, StateType
 from models.group import Group
 from tests.buzzn_test_case import BuzznTestCase
 from util.database import db
-from util.task import get_all_meter_ids, calc_end,\
+from util.task import get_all_meter_ids, calc_timestamps, calc_end,\
     calc_one_year_back, calc_support_year_start, calc_support_week_start,\
     calc_one_week_back, calc_two_days_back, client_name, Task
 
@@ -57,6 +57,52 @@ class TaskTestCase(BuzznTestCase):
         self.assertEqual(result, ALL_METER_IDS)
         for meter_id in result:
             self.assertEqual(len(meter_id), 32)
+
+    def test_calc_timestamps(self):
+        """ Unit tests for function calc_end().
+        Expect begin_ongoing_term to equal the start of the current support
+        year at 00:00:00 UTC.
+        Expect end_ongoing_term to equal today at 00:00:00 UTC.
+        Expect begin_previous_term to equal the start of the previous support
+        year at 00:00:00 UTC.
+        Expect end_previous_term to equal the end of the previous support year
+        at 00:00:00 UTC.
+        """
+
+        result = calc_timestamps()
+        today = datetime.today()
+
+        # Check result types
+        self.assertIsInstance(result, tuple)
+        self.assertEqual(len(result), 4)
+        for timestamp in result:
+            self.assertIsInstance(timestamp, int)
+
+        # Check result values
+        self.assertEqual(result[0], calc_support_year_start())
+        begin_ongoing_term = datetime.fromtimestamp(result[0]/1000)
+
+        end_ongoing_term = datetime.fromtimestamp(result[1]/1000)
+        self.assertEqual(end_ongoing_term.year, today.year)
+        self.assertEqual(end_ongoing_term.month, today.month)
+        self.assertEqual(end_ongoing_term.day, today.day)
+        for value in end_ongoing_term.hour, end_ongoing_term.minute, end_ongoing_term.second:
+            self.assertEqual(value, 0)
+
+        begin_previous_term = datetime.fromtimestamp(result[2]/1000)
+        self.assertEqual(begin_previous_term.year, begin_ongoing_term.year - 1)
+        self.assertEqual(begin_previous_term.month, 3)
+        self.assertEqual(begin_previous_term.day, 12)
+        for value in begin_previous_term.hour, begin_previous_term.minute,\
+                begin_previous_term.second:
+            self.assertEqual(value, 0)
+
+        end_previous_term = datetime.fromtimestamp(result[3]/1000)
+        self.assertEqual(end_previous_term.year, begin_ongoing_term.year)
+        self.assertEqual(end_previous_term.month, 3)
+        self.assertEqual(end_previous_term.day, 11)
+        for value in end_previous_term.hour, end_previous_term.minute, end_previous_term.second:
+            self.assertEqual(value, 0)
 
     def test_calc_end(self):
         """ Unit tests for function calc_end(). """
