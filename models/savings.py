48598ba1f3d940ccea956cc14eba22808202b055
Judith Greif <obermaedchen@gmail.com>
2020-02-12 14:56:14 +0100

    Add database models and migration file to create user and community saving tables, integrate them into app setup

---
+++ models/savings.py
@@ -0,0 +1,39 @@
+from sqlalchemy import ForeignKey
+from util.database import db
+
+
+class UserSaving(db.Model):
+    """ Represents a user savings entry in the backend. """
+
+    timestamp = db.Column(db.DateTime, primary_key=True)
+    id = db.Column(db.Integer, ForeignKey('user.id'))
+    meter_id = db.Column(db.String(32), ForeignKey('user.meter_id'))
+    saving = db.Column(db.Float)
+
+    def __init__(self, timestamp, user_id, meter_id, saving):
+        """ Creates a new user saving.
+        :param datetime.datetime timestamp: the UTC datetime
+        :param int id: the user's id
+        :param float saving: the user's saving
+        """
+
+        self.timestamp = timestamp
+        self.user_id = user_id
+        self.meter_id = meter_id
+        self.saving = saving
+
+
+class CommunitySaving(db.Model):
+    """ Represents a community savings entry in the backend. """
+
+    timestamp = db.Column(db.DateTime, primary_key=True)
+    saving = db.Column(db.Float)
+
+    def __init__(self, timestamp, saving):
+        """ Creates a new community saving.
+        :param datetime.datetime timestamp: the UTC datetime
+        :param float saving: the community saving
+        """
+
+        self.timestamp = timestamp
+        self.saving = saving
