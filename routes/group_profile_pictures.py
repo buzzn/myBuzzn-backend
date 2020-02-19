b021ef918d998efe70bb592077d1f1429a2579ad
Judith Greif <obermaedchen@gmail.com>
2020-02-19 14:53:04 +0100

    Intermediate state

--- routes/group_profile_pictures.py
+++ routes/group_profile_pictures.py
@@ -18,11 +18,11 @@ def get_group_members(user_id):
 
     target_user = User.query.filter_by(id=user_id).first()
     target_group = Group.query.filter_by(id=target_user.group_id).first()
-    group_members = User.query.filter_by(group_id=target_group.id).all()
-
-    print(target_user)
-    print(target_group)
-    print(group_members)
+    group_users = User.query.filter_by(group_id=target_group.id).all()
+    group_members = []
+    for group_user in group_users:
+        group_members.append(dict(id=group_user.id, avatar=group_user.avatar))
+    return group_members
 
 
 @GroupProfilePictures.route('/assets/group-profile-pictures', methods=['GET'])
