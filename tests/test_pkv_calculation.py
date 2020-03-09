e955ecfe11c2d30844538add55d286d031912b9c
Judith Greif <obermaedchen@gmail.com>
2020-03-09 13:55:04 +0100

    Adjust unit tests

--- tests/test_pkv_calculation.py
+++ tests/test_pkv_calculation.py
@@ -7,7 +7,7 @@ from tests.buzzn_test_case import BuzznTestCase
 from util.database import db
 from util.pkv_calculation import define_base_values, calc_pkv,\
     get_first_meter_reading_date, check_input_parameter_date,\
-    get_data_day_before_session
+    get_data_day_before
 
 
 SORTED_KEYS_DAY_ONE = [b'52d7c87f8c26433dbd095048ad30c8cf_2020-02-25 12:02:00',
@@ -40,17 +40,17 @@ RETURN_VALUES_DAY_ONE = ('2020-02-24 00:00:00.000000', '52d7c87f8c26433dbd095048
 RETURN_VALUES_DAY_TWO = ('2020-02-25 00:00:00.000000', '52d7c87f8c26433dbd095048ad30c8cf',
                          21.749714, 21.749714, 2, 10.874857, 10.874857, 1, 10.874857, 3969)
 
-BASE_VALUES = {'date': datetime(2020, 2, 25).date(), 'consumption': 0.0,
+BASE_VALUES = {'date': datetime(2020, 2, 25, 0, 0, 0), 'consumption': 0.0,
                'consumption_cumulated': 0.0, 'inhabitants': 2, 'pkv': 0.0,
                'pkv_cumulated': 0.0, 'days': 0, 'moving_average': 0.0,
                'moving_average_annualized': 0}
 
-PKV_DAY_ONE = {'date': datetime(2020, 2, 25).date(), 'consumption': 21.749714,
+PKV_DAY_ONE = {'date': datetime(2020, 2, 25, 0, 0, 0), 'consumption': 21.749714,
                'consumption_cumulated': 21.749714, 'inhabitants': 2,
                'pkv':  10.874857, 'pkv_cumulated': 10.874857,
                'days': 1, 'moving_average': 10.874857, 'moving_average_annualized': 3969}
 
-PKV_DAY_TWO = {'date': datetime(2020, 2, 26).date(), 'consumption': 15.0,
+PKV_DAY_TWO = {'date': datetime(2020, 2, 26, 0, 0, 0), 'consumption': 15.0,
                'consumption_cumulated': 36.749714, 'inhabitants': 2,
                'pkv': 7.5, 'pkv_cumulated': 18.374857, 'days': 2,
                'moving_average': 9.1874285, 'moving_average_annualized': 3353}
@@ -71,8 +71,8 @@ class PKVCalculationTestCase(BuzznTestCase):
         self.test_user.state = StateType.ACTIVE
         db.session.add(self.test_user)
 
-        self.day_zero = (datetime.today() - timedelta(days=2)).date()
-        self.day_one = (datetime.today() - timedelta(days=1)).date()
+        self.day_zero = (datetime.today() - timedelta(days=2))
+        self.day_one = (datetime.today() - timedelta(days=1))
         self.today = datetime.today().date()
 
         self.base_values = PKV(
@@ -91,8 +91,8 @@ class PKVCalculationTestCase(BuzznTestCase):
         """ Unit tests for function check_input_parameter_date().
         Expect False if the date to check lies in the future, True otherwise. """
 
-        good_date = datetime(2020, 1, 2).date()
-        bad_date = datetime.today().date() + timedelta(days=1)
+        good_date = datetime(2020, 1, 2, 0, 0, 0)
+        bad_date = datetime.today() + timedelta(days=1)
         result_good_date = check_input_parameter_date(good_date)
         result_bad_date = check_input_parameter_date(bad_date)
 
@@ -104,35 +104,34 @@ class PKVCalculationTestCase(BuzznTestCase):
         self.assertEqual(result_good_date, True)
         self.assertEqual(result_bad_date, False)
 
-    def test_get_data_day_before_session(self):
+    def test_get_data_day_before(self):
         """ Unit tests for function get_data_day_before(). """
 
-        data_day_zero = get_data_day_before_session(
+        data_day_zero = get_data_day_before(
             self.day_zero, self.test_user.meter_id, db.session)
-        data_day_one = get_data_day_before_session(
+        data_day_one = get_data_day_before(
             self.day_one, self.test_user.meter_id, db.session)
 
         # Check return types
-        self.assertIsInstance(data_day_zero, list)
-        self.assertIsInstance(data_day_one, list)
-        # self.assertIsInstance(data_day_before[0], str)
-        # self.assertIsInstance(data_day_before[1], str)
-        # self.assertIsInstance(data_day_before[2], float)
-        # self.assertIsInstance(data_day_before[3], float)
-        # self.assertIsInstance(data_day_before[4], int)
-        # self.assertIsInstance(data_day_before[5], float)
-        # self.assertIsInstance(data_day_before[6], float)
-        # self.assertIsInstance(data_day_before[7], int)
-        # self.assertIsInstance(data_day_before[8], float)
-        # self.assertIsInstance(data_day_before[9], int)
+        self.assertIsInstance(data_day_zero, type(None))
+        self.assertIsInstance(data_day_one.date, datetime)
+        self.assertIsInstance(data_day_one.consumption, float)
+        self.assertIsInstance(data_day_one.consumption_cumulated, float)
+        self.assertIsInstance(data_day_one.inhabitants, int)
+        self.assertIsInstance(data_day_one.pkv, float)
+        self.assertIsInstance(data_day_one.pkv_cumulated, float)
+        self.assertIsInstance(data_day_one.days, int)
+        self.assertIsInstance(data_day_one.moving_average, float)
+        self.assertIsInstance(data_day_one.moving_average_annualized, int)
 
-        # # Check return sizes
-        # self.assertEqual(len(data_day_before), 10)
+        # Check return values
+        self.assertEqual(data_day_zero, None)
+        self.assertEqual(data_day_one, self.base_values)
 
     def test_define_base_values(self):
         """ Unit tests for function define_base_values(). """
 
-        start = datetime(2020, 2, 26).date()
+        start = datetime(2020, 2, 26, 0, 0, 0)
         result = define_base_values(self.test_user.inhabitants, start)
 
         # Check return type
@@ -153,9 +152,9 @@ class PKVCalculationTestCase(BuzznTestCase):
     def test_calc_pkv(self, _get_meter_reading_date, _get, first):
         """ Unit tests for function calc_pkv(). """
 
-        start = datetime(2020, 2, 25).date()
+        start = datetime(2020, 2, 25, 0, 0, 0)
         result_day_one = calc_pkv(
-            self.test_user.meter_id, self.test_user.inhabitants, start)
+            self.test_user.meter_id, self.test_user.inhabitants, start, db.session)
 
         # Check result type
         self.assertIsInstance(result_day_one, (dict, type(None)))
@@ -174,9 +173,10 @@ class PKVCalculationTestCase(BuzznTestCase):
     @mock.patch('sqlalchemy.engine.result.ResultProxy.first', return_value=RETURN_VALUES_DAY_TWO)
     def test_calc_pkv_day_two(self, _get_meter_reading_date, _get, first):
         """ Unit tests for function calc_pkv() on day 2. """
-        day_two = datetime(2020, 2, 26).date()
+        day_two = datetime(2020, 2, 26, 0, 0, 0)
         result_day_two = calc_pkv(self.test_user.meter_id,
-                                  self.test_user.inhabitants, day_two)
+                                  self.test_user.inhabitants, day_two,
+                                  db.session)
 
         # Check result type
         self.assertIsInstance(result_day_two, (dict, type(None)))
