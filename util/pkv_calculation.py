3fecaef420bc52cf5e39c978a551226af16d59d1
Judith Greif <obermaedchen@gmail.com>
2020-03-09 13:35:08 +0100

    Documentation, switch database access method to session, refactor Python date usage

--- util/pkv_calculation.py
+++ util/pkv_calculation.py
@@ -7,7 +7,7 @@ import redis
 import pytz
 from sqlalchemy import extract
 from models.pkv import PKV
-from util.database import get_engine
+from util.database import create_session
 from util.energy_saving_calculation import get_last_meter_reading_date
 from util.error import exception_message
 from util.redis_helpers import get_sorted_keys
@@ -44,45 +44,13 @@ def check_input_parameter_date(date):
     :return: True if date does not lie in the future, False otherwise
     """
 
-    today = datetime.utcnow().date()
-    if date > today:
+    today = datetime.utcnow()
+    if date.date() > today.date():
         return False
     return True
 
 
-def get_data_day_before(date, meter_id):
-    """ Get the values from the day before from the SQLite database.
-    :param datetime.date: the request date
-    :param str meter_id: the user's meter id
-    :return: date, meter_id, consumption, consumption_cumulated, inhabitants,
-    pkv, pkv_cumulated, days, moving_average and moving_average_annualized
-    """
-
-    day_before = date - timedelta(days=1)
-    day_before_str = str(day_before) + ' 00:00:00.000000'
-
-    try:
-        # Connect to sqlite database
-        engine = get_engine()
-        with engine.connect() as con:
-
-            # pylint: disable=fixme
-            # TODO - use SQLAlchemy queries
-            # pylint: disable=line-too-long
-            query_str = (
-                "SELECT * FROM PKV WHERE meter_id = '%s' AND date = '%s'" %
-                (meter_id, day_before_str))
-
-            result = con.execute(query_str).first()
-            return result
-
-    except Exception as e:
-        message = exception_message(e)
-        logger.error(message)
-        return None
-
-
-def get_data_day_before_session(dt, meter_id, session):
+def get_data_day_before(dt, meter_id, session):
     """ Get the values from the day before from the SQLite database.
     :param datetime.date: the request date
     :param str meter_id: the user's meter id
@@ -92,12 +60,14 @@ def get_data_day_before_session(dt, meter_id, session):
     """
 
     day_before = dt - timedelta(days=1)
+
     try:
         result = session.query(PKV).filter_by(
             meter_id=meter_id).filter(extract('year', PKV.date) == day_before.year,
                                       extract(
                                           'month', PKV.date) == day_before.month,
-                                      extract('day', PKV.date) == day_before.day).all()
+                                      extract('day', PKV.date) ==
+                                      day_before.day).first()
 
         return result
 
@@ -202,7 +172,7 @@ def define_base_values(inhabitants, date):
     return base_values
 
 
-def calc_pkv(meter_id, inhabitants, date):
+def calc_pkv(meter_id, inhabitants, date, session):
     """ Calculate the Pro-Kopf-Verbrauch for a given user on a given date.
     :param str meter_id: the user's meter id
     :param int inhabitants: the number of inhabitants in the user's flat
@@ -220,6 +190,7 @@ def calc_pkv(meter_id, inhabitants, date):
 
     # Calculate consumption := last meter reading of date - first meter reading
     # of date in kWh
+
     consumption_mywh_last = get_last_meter_reading_date(meter_id, date)
     consumption_mywh_first = get_first_meter_reading_date(meter_id, date)
 
@@ -229,7 +200,7 @@ def calc_pkv(meter_id, inhabitants, date):
     consumption = (consumption_mywh_last - consumption_mywh_first)/1e9
 
     # Retrieve data for the day before from the SQLite database
-    data_day_before = get_data_day_before(date, meter_id)
+    data_day_before = get_data_day_before(date, meter_id, session)
 
     if data_day_before is None:
         logger.info(
@@ -240,18 +211,18 @@ def calc_pkv(meter_id, inhabitants, date):
     try:
         # Calculate consumption_cumulated := consumption_cumulated of the day
         # before + consumption (kWh)
-        consumption_cumulated = data_day_before[2] + consumption
+        consumption_cumulated = data_day_before.consumption_cumulated + consumption
 
         # Calculate pkv (Pro-Kopf-Verbrauch) := consumption/inhabitants (kWh)
         pkv = consumption/inhabitants
 
         # Calculate pkv_cumulated := pkv_cumulated of the day
         # before + pkv (kWh)
-        pkv_cumulated = data_day_before[6] + pkv
+        pkv_cumulated = data_day_before.pkv_cumulated + pkv
 
         # Calculate days (since calculation start) := days of the day before + 1
         # (number)
-        days = data_day_before[7] + 1
+        days = data_day_before.days + 1
 
     except Exception as e:
         message = exception_message(e)
