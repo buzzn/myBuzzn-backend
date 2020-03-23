742337cb887da310ff05ec7cd7285ef124ea6d41
KatharinaGrett <katharina-grett@web.de>
2020-03-23 09:52:34 +0100

    changing context

--- util/mailer.py
+++ util/mailer.py
@@ -22,12 +22,11 @@ def send_mail(receiver, message):
         print(message)
         print("<<<mailend")
     elif app.config['BUZZN_MAILER'] == 'smtp':
-        context = ssl.create_default_context()
+        #context = ssl.create_default_context()
         logger.error(app.config['BUZZN_SMTP_SERVER'])
         logger.error(app.config['BUZZN_SMTP_SERVER_PORT'])
         with smtplib.SMTP_SSL(app.config['BUZZN_SMTP_SERVER'],
-                              app.config['BUZZN_SMTP_SERVER_PORT'],
-                              context=context) as server:
+                              app.config['BUZZN_SMTP_SERVER_PORT']) as server:
             server.login(app.config['BUZZN_EMAIL'],
                          app.config['BUZZN_EMAIL_PASSWORD'])
             server.sendmail(app.config['BUZZN_EMAIL'], receiver, message)
