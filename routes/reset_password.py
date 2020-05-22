08bf8f219cb1da39c741c4c71a0aa6c007c79e9d
KatharinaGrett <katharina-grett@web.de>
2020-05-22 12:17:12 +0200

    used MimeText

--- routes/reset_password.py
+++ routes/reset_password.py
@@ -2,7 +2,8 @@ from flask import render_template, Blueprint, Response, request
 from flask import current_app as app
 from flask_api import status
 
-from models.user import User, StateType, PASSWORD_MAX_LENGTH
+from models.user import User, StateType
+# , PASSWORD_MAX_LENGTH
 from util.database import db
 from util.error import Error
 from util.translation import get_opening_greeting
@@ -36,14 +37,12 @@ def request_password_reset_token():
 
     target_user.generate_password_request_token()
 
-    send_mail(
-        target_user.mail,
-        render_template("password/reset_password_mail.txt",
-                        greeting=get_opening_greeting(target_user),
-                        wlink="{}/password/reset/{}".format(
-                            app.config['BUZZN_BASE_URL'],
-                            target_user.password_reset_token
-                        )))
+    send_mail(target_user.mail, render_template("password/reset_password_mail.txt",
+                                                greeting=get_opening_greeting(target_user),
+                                                wlink="{}/password/reset/{}".format(
+                                                    app.config['BUZZN_BASE_URL'],
+                                                    target_user.password_reset_token
+                                                )), 'Passwort zurücksetzen für Buzzn-App')
 
     db.session.commit()
     return '', status.HTTP_201_CREATED
@@ -79,7 +78,7 @@ def do_password(token):
                                                  'angefordert haben und nehmen sie '
                                                  'immer die aktuelle.')))
 
-    if target_user.state != StateType.PASSWORT_RESET_PENDING:
+    if not target_user.state == StateType.PASSWORT_RESET_PENDING:
         return Response(render_template('password/failure.html',
                                         message='User has no pending password reset.'))
 
@@ -88,12 +87,12 @@ def do_password(token):
                                         passwordResetToken=token,
                                         message='Passwörter stimmen nicht überein.'))
 
-    if not target_user.check_password_length(requested_password):
-        return Response(render_template('password/request.html',
-                                        passwordResetToken=token,
-                                        message=('Passwort zu kurz. Das '
-                                                 'Passwort muss mindestens {} '
-                                                 'Zeichen haben').format(PASSWORD_MAX_LENGTH)))
+    #if not target_user.check_password_length(requested_password):
+     #   return Response(render_template('password/request.html',
+      #                                  passwordResetToken=token,
+       #                                 message=('Passwort zu kurz. Das '
+        #                                         'Passwort muss mindestens {} '
+         #                                        'Zeichen haben').format(PASSWORD_MAX_LENGTH)))
 
     target_user.set_password(requested_password)
     target_user.state = StateType.ACTIVE
--- util/mailer.py
+++ util/mailer.py
@@ -1,4 +1,6 @@
 import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
 import logging.config
 
 from flask import current_app as app
@@ -7,10 +9,11 @@ from flask import current_app as app
 logger = logging.getLogger(__name__)
 
 
-def send_mail(receiver, message):
+def send_mail(receiver, message, subject):
     """Sends a new mail to the given receiver.
     :param str receiver: The receiver's mail address.
     :param str message: The message.
+    :param str subject: The subject of the mail.
     :raises AssertionError: If no mailer is configured i.e. BUZZN_MAILER is not
     set.
     """
@@ -21,13 +24,21 @@ def send_mail(receiver, message):
         print(message)
         print("<<<mailend")
     elif app.config['BUZZN_MAILER'] == 'smtp':
-
-        message_as_bytes = message.encode('ascii', 'replace')
         with smtplib.SMTP_SSL(app.config['BUZZN_SMTP_SERVER'],
                               app.config['BUZZN_SMTP_SERVER_PORT']) as server:
+            msg = MIMEText(message, 'plain', 'utf-8')
+            msg['Subject'] = Header(subject, 'utf-8')
+            msg['From'] = app.config['BUZZN_EMAIL']
+            msg['To'] = receiver
             server.login(app.config['BUZZN_EMAIL'],
                          app.config['BUZZN_EMAIL_PASSWORD'])
-            server.sendmail(app.config['BUZZN_EMAIL'], receiver, message_as_bytes)
+            server.send_message(msg)
+        # message_as_bytes = message.encode('ascii', 'replace')
+       # with smtplib.SMTP_SSL(app.config['BUZZN_SMTP_SERVER'],
+        #                      app.config['BUZZN_SMTP_SERVER_PORT']) as server:
+         #   server.login(app.config['BUZZN_EMAIL'],
+          #               app.config['BUZZN_EMAIL_PASSWORD'])
+           # server.sendmail(app.config['BUZZN_EMAIL'], receiver, message_as_bytes)
         logger.info("Password reset message sent.")
     else:
         raise AssertionError("BUZZN_MAILER not set, no mailer configured. Cannot send mail.")
